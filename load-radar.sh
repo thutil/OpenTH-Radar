219560ed3ba160de695cf6d8780f67ce2f239c4f
bankjirapan <jirapan_yankhan@hotmail.com>
2025-08-22 18:01:24 +0700

    feat: add script to download radar images and create necessary directories

--- load-radar.sh
+++ load-radar.sh
@@ -10,8 +10,20 @@ filenames=("chn/chn240_HQ_latest.png" "phs/phs240_HQ_latest.png" "cri/cri240_HQ_
 currentTime=$(date +%s)
 roundedTime=$(( (currentTime / 600) * 600 ))
 
+printf "Rounded time: %d\n" "$roundedTime"
+
+# Check if radar_img directory exists, if not, create it
+if [ ! -d "./radar_img" ]; then
+    mkdir -p "./radar_img"
+fi
+
+# Check if timestamp directory exists, if not, create it
+if [ ! -d "./radar_img/$roundedTime" ]; then
+    mkdir -p "./radar_img/$roundedTime"
+fi
+
 # Directory to save images (rounded Unix time)
-outputDir="../raw_radar/$roundedTime"
+outputDir="./radar_img/$roundedTime"
 
 # Create output directory if it doesn't exist
 mkdir -p "$outputDir"
